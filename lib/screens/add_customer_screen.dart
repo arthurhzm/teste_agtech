a629bca2ba3d8a88e6c6f27e8d641cd26e5393eb
Arthur Marena <arthurzhm2@gmail.com>
2025-07-02 15:03:21 -0300

    FEAT: Criando primeiras paginas

---
+++ lib/screens/add_customer_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddCustomerScreen extends StatefulWidget {
+  const AddCustomerScreen({super.key});
+
+  @override
+  State<AddCustomerScreen> createState() => _AddCustomerScreenState();
+}
+
+class _AddCustomerScreenState extends State<AddCustomerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+    )
+  }
+}
